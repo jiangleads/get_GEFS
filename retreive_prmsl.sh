957ba3bbf552a23e47a9fa39053f9c9546cdc6c1
Lizhi Jiang <16336860+jiangleads@users.noreply.github.com>
2024-09-30 16:28:56 -0500

    scripts to extract GFS variable

---
+++ retreive_prmsl.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+#
+# example of calculating GFS APCP for custom periods
+#  want 06-09, 09-12, 12-15, 15-24, 24-48 hours
+#  need the 06, 09, 12, 15, 24 and 48 hour forecasts
+#
+# the output iis in apcp.grb
+# the calculated APCP fields may have a small negative because of
+# finite precision is used
+
+dir=$1
+echo $dir
+cat   $dir/gfs.t00z.pgrb2.0p25.f001 $dir/gfs.t00z.pgrb2.0p25.f002 \
+      $dir/gfs.t00z.pgrb2.0p25.f003 $dir/gfs.t00z.pgrb2.0p25.f004 \
+      $dir/gfs.t00z.pgrb2.0p25.f005 $dir/gfs.t00z.pgrb2.0p25.f006 \
+      $dir/gfs.t06z.pgrb2.0p25.f001 $dir/gfs.t06z.pgrb2.0p25.f002 \
+      $dir/gfs.t06z.pgrb2.0p25.f003 $dir/gfs.t06z.pgrb2.0p25.f004 \
+      $dir/gfs.t06z.pgrb2.0p25.f005 $dir/gfs.t06z.pgrb2.0p25.f006 \
+      $dir/gfs.t12z.pgrb2.0p25.f001 $dir/gfs.t12z.pgrb2.0p25.f002 \
+      $dir/gfs.t12z.pgrb2.0p25.f003 $dir/gfs.t12z.pgrb2.0p25.f004 \
+      $dir/gfs.t12z.pgrb2.0p25.f005 $dir/gfs.t12z.pgrb2.0p25.f006 \
+      $dir/gfs.t18z.pgrb2.0p25.f001 $dir/gfs.t18z.pgrb2.0p25.f002 \
+      $dir/gfs.t18z.pgrb2.0p25.f003 $dir/gfs.t18z.pgrb2.0p25.f004 \
+      $dir/gfs.t18z.pgrb2.0p25.f005 $dir/gfs.t18z.pgrb2.0p25.f006 \
+      |  \wgrib2 - -match ":PRMSL:" -match ":[1-6]* hour fcst:" -set_grib_type c1 -grib prmsl_$1.grb
---
+++ retreive_v10m.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#
+# example of calculating GFS APCP for custom periods
+#  want 06-09, 09-12, 12-15, 15-24, 24-48 hours
+#  need the 06, 09, 12, 15, 24 and 48 hour forecasts
+#
+# the output iis in apcp.grb
+# the calculated APCP fields may have a small negative because of
+# finite precision is used
+
+dir=$1
+echo $dir
+cat   $dir/gfs.t00z.pgrb2.0p25.f001 $dir/gfs.t00z.pgrb2.0p25.f002 \
+      $dir/gfs.t00z.pgrb2.0p25.f003 $dir/gfs.t00z.pgrb2.0p25.f004 \
+      $dir/gfs.t00z.pgrb2.0p25.f005 $dir/gfs.t00z.pgrb2.0p25.f006 \
+      $dir/gfs.t06z.pgrb2.0p25.f001 $dir/gfs.t06z.pgrb2.0p25.f002 \
+      $dir/gfs.t06z.pgrb2.0p25.f003 $dir/gfs.t06z.pgrb2.0p25.f004 \
+      $dir/gfs.t06z.pgrb2.0p25.f005 $dir/gfs.t06z.pgrb2.0p25.f006 \
+      $dir/gfs.t12z.pgrb2.0p25.f001 $dir/gfs.t12z.pgrb2.0p25.f002 \
+      $dir/gfs.t12z.pgrb2.0p25.f003 $dir/gfs.t12z.pgrb2.0p25.f004 \
+      $dir/gfs.t12z.pgrb2.0p25.f005 $dir/gfs.t12z.pgrb2.0p25.f006 \
+      $dir/gfs.t18z.pgrb2.0p25.f001 $dir/gfs.t18z.pgrb2.0p25.f002 \
+      $dir/gfs.t18z.pgrb2.0p25.f003 $dir/gfs.t18z.pgrb2.0p25.f004 \
+      $dir/gfs.t18z.pgrb2.0p25.f005 $dir/gfs.t18z.pgrb2.0p25.f006 \
+      |  \wgrib2 - -match ":VGRD:" -match ":10 m above ground:" -match ":* hour fcst:" -set_grib_type c1 -grib v10m_$1.grb
